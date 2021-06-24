2710f55b80f38a4a3d2ef9a85fa1f81c5a8ef988
Daniel Buriticá <dani.ferbu@hotmail.com>
2021-06-24 06:43:25 -0500

    CSS Card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 export default function Card({id, title, image, diets}) {
 	return (
 		<React.Fragment>
-			<div>
-				<div>
-					<img src={image} alt=""/>
+			<div className='card'>
+				<div className='img-recipe'>
+					<img src={image} alt={title}/>
 				</div>
-				<div>
+				<div className='card-info'>
 					<Link to={`/recipes/${id}`}>
-	                	<h2>{title}</h2>
+	                	<h2 className='card-title'>{title}</h2>
 	                </Link>
-					<h4>{diets}</h4>
+					<h4 className='card-diets'>{diets}</h4>
 				</div>
 			</div>
     	</React.Fragment>
